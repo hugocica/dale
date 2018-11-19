73a64c90f5931a6dc6620a4f530e220dee9bfcdd
Cainen <caina_guari@hotmail.com>
2018-11-19 20:56:13 -0200

    Teste

--- src/components/pages/relatorio/index.jsx
+++ src/components/pages/relatorio/index.jsx
@@ -26,7 +26,7 @@ class RelatorioIndex extends Component {
                             </Book.Side>
 
                             <Book.Side>
-                                <div className="book-page left red-bg white-text vertical-aligned">
+                                <div className="book-page left red-bg white-text">
                                     <p className="citacao">Pode-se resumir a música eletrônica como "A música produzida a partir de não-instrumentos, ou de instrumentos adaptados para produzir som modificado pela eletricidade".</p>
                                 </div>
                             </Book.Side>
@@ -44,19 +44,14 @@ class RelatorioIndex extends Component {
                                         Com a popularização do estilo musical no Brasil e no mundo, principalmente a partir dos anos 2000, os dados econômicos relacionados ao movimento surpreendem. Segundo dados do site de jornalismo G1, em 2014, mais de 28 milhões de brasileiros disseram ter ouvido algum tipo de música eletrônica. Além disso, a indústria movimentou mais de R$ 3,1 bilhões no país entre shows e comércio relacionado a esse tipo de entretenimento.
                                     </p>
 
-                                    <p>
-                                        Com essa grande movimentação econômica e o surgimento de grandes nomes brasileiros no meio musical como Alok e Vintage Culture, o Brasil ganha lugar de destaque e atrai grande festivais internacionais do gênero, fazendo com que a receita gerada seja ainda maior do que a apresentada há 4 anos, além de ganhar cada vez mais importância nesse tipo de cenário.
-                                    </p>
-
-                                    <p>
-                                        Com essa grande movimentação econômica e o surgimento de grandes nomes brasileiros no meio musical como Alok e Vintage Culture, o Brasil ganha lugar de destaque e atrai grande festivais internacionais do gênero, fazendo com que a receita gerada seja ainda maior do que a apresentada há 4 anos, além de ganhar cada vez mais importância nesse tipo de cenário.
-                                    </p>
                                 </div>
                             </Book.Side>
 
                             <Book.Side>
                                 <div className="book-page left">
-
+                                    <p>
+                                        Com essa grande movimentação econômica e o surgimento de grandes nomes brasileiros no meio musical como Alok e Vintage Culture, o Brasil ganha lugar de destaque e atrai grande festivais internacionais do gênero, fazendo com que a receita gerada seja ainda maior do que a apresentada há 4 anos, além de ganhar cada vez mais importância nesse tipo de cenário.
+                                    </p>
                                 </div>
                             </Book.Side>
                         </Book.Page>
@@ -78,10 +73,13 @@ class RelatorioIndex extends Component {
 
                             <Book.Side>
                                 <div className="book-page left">
-                                <p className="subtitle"><strong>1. A música eletrônica e o que ela tem a ver com este projeto</strong></p>
+                                <p className="subtitle">
+                                    <strong>1. A música eletrônica e o que ela tem a ver com este projeto</strong>
+                                </p>
 
                                 <p>
                                     A música eletrônica tem seus primórdios com Pierre Schaeffer, na França em 1948 com músicas feitas a partir de ruídos de toca-discos, também chamado na época de “musique concrète”. Porém só se popularizou com o surgimento dos computadores pessoais no início dos anos 90, com a facilitação das produções musicais sem a necessidade da estrutura de um estúdio profissional, mas com resultados se mostrando similares à qualidade dos mesmos. Com isso, muitos produtores musicais e, consequentemente, seus seguidores e ouvintes começaram a surgir e dar corpo ao gênero musical que hoje chamamos de música eletrônica e seu movimento que abarca um público extremamente grande e cada vez maior.
+                                    
                                 </p>
                                 <p>
 
@@ -92,8 +90,17 @@ class RelatorioIndex extends Component {
 
 
                         {/*
-                          <br /> => quebra de linha
-                          <em></em>
+                          <br />                  => quebra de linha
+                          <em></em>               => italico
+                          <strong></strong>       => negrito
+
+                          <p className="title">
+                          </p>
+
+                          className=""            => adicionar classes no elemento
+                                title
+                                subtitle
+                                citacao
 
                           <Book.Page>
                               <Book.Side>
